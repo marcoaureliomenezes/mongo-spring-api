405fdbf275a1837db31a00e285633800e3841349
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-26 18:23:50 -0300

    first commit. Default spring boot project using 8081 port

---
+++ src/main/java/com/marcomenezes/mongoDBspringapi/MongoSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcomenezes.mongoDBspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcomenezes/mongoDBspringapi/MongoSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcomenezes.mongoDBspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
