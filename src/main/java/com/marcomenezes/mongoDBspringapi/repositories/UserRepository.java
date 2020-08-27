6e59e535b033872e24273f54428751894ffe40a8
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-27 16:56:38 -0300

    Connection to mongoDB. UserRepository and UserService created

---
+++ src/main/java/com/marcomenezes/mongoDBspringapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.marcomenezes.mongoDBspringapi.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.marcomenezes.mongoDBspringapi.domain.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/marcomenezes/mongoDBspringapi/services/UserService.java
@@ -0,0 +1,20 @@
+package com.marcomenezes.mongoDBspringapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcomenezes.mongoDBspringapi.domain.User;
+import com.marcomenezes.mongoDBspringapi.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
