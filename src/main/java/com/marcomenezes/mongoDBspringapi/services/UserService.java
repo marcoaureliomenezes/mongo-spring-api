d5f0ba4d11cac4846e97642e76045cbf11eaed0e
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-27 22:34:39 -0300

    Inserting users with the method post

--- src/main/java/com/marcomenezes/mongoDBspringapi/services/UserService.java
+++ src/main/java/com/marcomenezes/mongoDBspringapi/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcomenezes.mongoDBspringapi.domain.User;
+import com.marcomenezes.mongoDBspringapi.dto.UserDTO;
 import com.marcomenezes.mongoDBspringapi.repositories.UserRepository;
 import com.marcomenezes.mongoDBspringapi.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
