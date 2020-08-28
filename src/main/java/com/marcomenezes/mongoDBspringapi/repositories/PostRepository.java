5eee83950802728cb0a622195a31bab923c01892
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-28 15:55:57 -0300

    Simple query to search a post using a fragment of the title (query methods)

--- src/main/java/com/marcomenezes/mongoDBspringapi/repositories/PostRepository.java
+++ src/main/java/com/marcomenezes/mongoDBspringapi/repositories/PostRepository.java
@@ -1,9 +1,15 @@
 package com.marcomenezes.mongoDBspringapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.marcomenezes.mongoDBspringapi.domain.Post;
 
+
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
