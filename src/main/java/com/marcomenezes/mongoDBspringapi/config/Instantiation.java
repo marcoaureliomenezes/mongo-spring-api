5181f79186ec0f4318f74a2d600dbf06e3f7f7c7
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-08-28 00:52:07 -0300

    Adding comments to post entity

--- src/main/java/com/marcomenezes/mongoDBspringapi/config/Instantiation.java
+++ src/main/java/com/marcomenezes/mongoDBspringapi/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.marcomenezes.mongoDBspringapi.domain.Post;
 import com.marcomenezes.mongoDBspringapi.domain.User;
 import com.marcomenezes.mongoDBspringapi.dto.AuthorDTO;
+import com.marcomenezes.mongoDBspringapi.dto.CommentDTO;
 import com.marcomenezes.mongoDBspringapi.repositories.PostRepository;
 import com.marcomenezes.mongoDBspringapi.repositories.UserRepository;
 
@@ -46,10 +47,19 @@ public class Instantiation implements CommandLineRunner {
 				"Deu certo!", "Graças a Deus consegui um emprego!!!",
 				new AuthorDTO(user4));
 
-		user4.getPosts().addAll(Arrays.asList(post1, post2));
+		CommentDTO c1 = new CommentDTO("Parabéns. Vai se dar bem demais lá.", sdf.parse("03/09/2020"), new AuthorDTO(user3));
+		CommentDTO c2 = new CommentDTO("Boa sorte lá mano. Sucesso!!!", sdf.parse("03/09/2020"), new AuthorDTO(user2));
+		CommentDTO c3 = new CommentDTO("Aí deu certo. É nois.", sdf.parse("05/09/2020"), new AuthorDTO(user1));
+		CommentDTO c4 = new CommentDTO("Até que enfim hein", sdf.parse("28/08/2020"), new AuthorDTO(user3));
+		CommentDTO c5 = new CommentDTO("Aí sim hein.", sdf.parse("29/08/2020"), new AuthorDTO(user1));
+		
+		post1.getComments().addAll(Arrays.asList(c4, c5));
+		post2.getComments().addAll(Arrays.asList(c1, c2, c3));
+		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		user4.getPosts().addAll(Arrays.asList(post1, post2));
 		
 		userRepository.save(user4);
 	}
--- src/main/java/com/marcomenezes/mongoDBspringapi/domain/Post.java
+++ src/main/java/com/marcomenezes/mongoDBspringapi/domain/Post.java
@@ -1,13 +1,15 @@
 package com.marcomenezes.mongoDBspringapi.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.marcomenezes.mongoDBspringapi.dto.AuthorDTO;
-import com.marcomenezes.mongoDBspringapi.dto.UserDTO;
+import com.marcomenezes.mongoDBspringapi.dto.CommentDTO;
 
 
 @Document
@@ -20,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -75,6 +79,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -99,5 +111,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 
 }
